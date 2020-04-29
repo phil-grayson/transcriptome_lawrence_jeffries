9abf2c7e30c281a10a39dc10feb4b975b7bd701d
Phil Grayson <pgrayson@gra-login2.graham.sharcnet>
2020-04-29 17:51:42 -0400

    moving bowtie files to super_transcriptome

---
+++ lamprey/super_transcriptome/bowtie2_8_long.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J Bowtie2 
+#SBATCH -N 1                      # Ensure that all cores are on one machine
+#SBATCH -n 8                # Use n cores for one job 
+#SBATCH -t 3-00:00                # Runtime in D-HH:MM 
+#SBATCH --mem=28000            # Memory pool for all cores 
+#SBATCH -o bt2.%A.out       # File to which STDOUT will be written 
+#SBATCH -e bt2.%A.err       # File to which STDERR will be written 
+#SBATCH --account=wgf-720-aa
+
+# This was run on Grex
+
+module load gcc/5.2
+module load intel/14.0.2.144
+module load samtools/1.9
+
+/home/pgrayson/programs/bowtie2-2.3.5.1-linux-x86_64/bowtie2 -x $3 -1 $1 -2 $2 -X 2000 -p 8 --all | samtools view -b -S - |samtools sort - -o $1.bam
+
+samtools index $1.bam
